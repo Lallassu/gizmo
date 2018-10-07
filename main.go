c2e7f34d6cfff77014a1590497711074a38366f5
Magnus Persson <Magnus.Persson@ipeer.se>
2018-10-07 10:58:57 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"math"
+	"time"
+)
+
+//=============================================================
+// Main
+//=============================================================
+func main() {
+	pixelgl.Run(run)
+}
+
+//=============================================================
+// Setup game window etc.
+//=============================================================
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:       GameTitle,
+		Bounds:      pixel.R(0, 0, 1024, 768),
+		VSync:       Vsync,
+		Undecorated: Undecorated,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup world etc.
+	setup()
+
+	// Start game loop
+	gameLoop(win)
+}
+
+//=============================================================
+// Setup map, world, player etc.
+//=============================================================
+func setup() {
+	// Init map
+
+	// Init
+
+	//
+}
+
+//=============================================================
+// Game loop
+//=============================================================
+func gameloop(win *pixelgl.Window) {
+	last := time.Now()
+	//frames := 0
+	fps := time.Tick(time.Second / 60)
+	second := time.Tick(time.Second)
+
+	for !win.Closed() {
+		win.Update()
+		<-fps
+		//updateFPSDisplay(win, &frames, second)
+	}
+}
---
+++ types.go
@@ -0,0 +1,19 @@
+package main
+
+type mapType int
+
+const (
+	map_easy mapType = iota
+	map_normal
+	map_hard
+	map_hell
+	map_wtf
+)
+
+type weaponType int
+
+const (
+	weapon_ak47 weaponType = iota
+	weapon_rocket
+	weapon_grenade
+)
