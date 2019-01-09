fb15ebd18b93552e9bbf038f7a2f9382c1b78f61
Magnus Persson <Magnus.Persson@ipeer.se>
2019-01-09 12:48:01 +0100

    Added ammobar

--- weapon.go
+++ weapon.go
@@ -7,7 +7,8 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/faiface/pixel"
+	"github.com/faiface/pixel"
+	"image"
 )
 
 //=============================================================
@@ -15,12 +16,16 @@ import (
 //=============================================================
 type weapon struct {
 	object
-	shot       ammo
-	wType      objectType
-	bullets    int
-	spread     float64
-	reload     float64
-	reloadTime float64
+	shot             ammo
+	wType            objectType
+	bullets          int
+	spread           float64
+	reload           float64
+	reloadTime       float64
+	ammoBar          *pixel.Sprite
+	ammoImg          *pixel.PictureData
+	ammoCount        int
+	currentAmmoCount int
 }
 
 //=============================================================
@@ -29,8 +34,13 @@ type weapon struct {
 func (w *weapon) newWeapon(x, y float64, wType objectType) {
 	w.reloadTime = 0
 	w.wType = wType
+
+	ammoBarType := "ammobar.png"
+
 	switch wType {
 	case weaponAk47:
+		w.ammoCount = 50
+		ammoBarType = "ammobar.png"
 		w.sheetFile = fmt.Sprintf("%v%v", wAssetObjectsPath, "ak47_weapon.png")
 		w.name = "ak47"
 		w.animated = false
@@ -48,6 +58,8 @@ func (w *weapon) newWeapon(x, y float64, wType objectType) {
 		w.bullets = 1
 		w.reload = 0.05
 	case weaponShotgun:
+		w.ammoCount = 20
+		ammoBarType = "ammobar.png"
 		w.sheetFile = fmt.Sprintf("%v%v", wAssetObjectsPath, "shotgun_weapon.png")
 		w.animated = false
 		w.rotation = 0.1
@@ -67,11 +79,19 @@ func (w *weapon) newWeapon(x, y float64, wType objectType) {
 	}
 	w.create(x, y)
 
+	// Create HP Bar
+	var img image.Image
+	img, _, _, _ = loadTexture(fmt.Sprintf("%v%v", wAssetMixedPath, ammoBarType))
+	w.ammoImg = pixel.PictureDataFromImage(img)
+	w.ammoBar = pixel.NewSprite(w.ammoImg, pixel.R(0, 0, 40, 5))
+
 	// Animate up/down when idle
-	w.animateIdle = true
+	w.animateIdle = false
 
 	// Must change entity type in bounds for QT lookup
 	w.bounds.entity = Entity(w)
+
+	w.currentAmmoCount = w.ammoCount
 }
 
 //=============================================================
@@ -80,6 +100,13 @@ func (w *weapon) newWeapon(x, y float64, wType objectType) {
 func (w *weapon) draw(dt, elapsed float64) {
 	w.reloadTime += dt
 	w.object.draw(dt, elapsed)
+
+	// Draw ammobar
+	if w.owner == nil {
+		w.ammoBar.Draw(global.gWin, pixel.IM.Scaled(pixel.ZV, 0.3).Moved(pixel.V(w.bounds.X+w.bounds.Width/2, w.bounds.Y+w.bounds.Height+5)))
+	} else {
+		w.ammoBar.Draw(global.gWin, pixel.IM.Scaled(pixel.ZV, 0.3).Moved(pixel.V(w.owner.bounds.X+w.owner.bounds.Width/2, w.owner.bounds.Y+w.owner.bounds.Height+7)))
+	}
 }
 
 //=============================================================
@@ -90,6 +117,13 @@ func (w *weapon) shoot() {
 	if w.owner == nil {
 		return
 	}
+	if w.currentAmmoCount <= 0 {
+		return
+	}
+
+	w.currentAmmoCount--
+	w.ammoBar.Set(w.ammoImg, pixel.R(0, 0, 40*(float64(w.currentAmmoCount)/float64(w.ammoCount)), 5))
+
 	if w.reloadTime > w.reload {
 		//global.gSounds.play("shot")
 		w.rotation = 0.1
